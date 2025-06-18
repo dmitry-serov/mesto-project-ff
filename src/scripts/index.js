82a67dd8dacdd8f53d249c2e9bd18e702959e231
dmitry-serov <frickmen@yandex.ru>
2025-06-18 23:26:24 +0300

    Открытие и закрытие модального окна

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,7 +8,24 @@ import { openModal, closeModal } from './modal.js';
 const cardsList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
+const profileAddButton = document.querySelector('.profile__add-button');
+const profileEditButton = document.querySelector('.profile__edit-button');
+
+const modalNewCard = document.querySelector('.popup_type_new-card');
+const modalEditProfile = document.querySelector('.popup_type_edit');
+const modalImage = document.querySelector('.popup_type_image');
+
 initialCards.forEach((cardData) => {
     const cardElement = createCardElement(cardData, cardTemplate, deleteCardElement);
     cardsList.append(cardElement);
+});
+
+profileAddButton.addEventListener('click', () => openModal(modalNewCard));
+profileEditButton.addEventListener('click', () => openModal(modalEditProfile));
+cardsList.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('card__image')) {
+        const popupImage = document.querySelector('.popup__image');
+        popupImage.src = evt.target.src;
+        openModal(modalImage);
+    }
 });
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,7 +1,9 @@
-export function openModal() {
-
+export function openModal(modalElement) {
+    modalElement.classList.add('popup_is-opened');
+    const closeIcon = modalElement.querySelector('.popup__close');
+    closeIcon.addEventListener('click', () => closeModal(modalElement));
 }
 
-export function closeModal() {
-
+export function closeModal(modalElement) {
+    modalElement.classList.remove('popup_is-opened');
 }
